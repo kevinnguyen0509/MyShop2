0c2a57a7049acdec2f957421ff6855fa3f22d9a5
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2021-03-29 10:59:32 -0700

    Fixed update in memory class

---
+++ MyShop/MyShop.WebUI2/Controllers/ProductManagerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace MyShop.WebUI2.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+        // GET: ProductManager
+
+        public ProductManagerController() {
+            context = new ProductRepository();
+        }
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create() {
+            Product product = new Product();
+            return View(product);
+        }
+
+        public ActionResult Create(Product product) {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else {
+                context.Insert(product);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
